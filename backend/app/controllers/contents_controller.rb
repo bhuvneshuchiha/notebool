55dfeedba49fdae71cd9c880008b5f5641fb24a0
bhuvnesh <bhuvnesh.awasthi@gmail.com>
2025-10-23 02:06:15 +0530

    crud for contentscontroller

--- backend/app/controllers/contents_controller.rb
+++ backend/app/controllers/contents_controller.rb
@@ -30,10 +30,77 @@ class ContentsController < ApplicationController
       )
       return
     else
-      render json: {
+      render(
+        json: {
           message: "Data could not be saved",
           error: content.errors.full_messages
         }
+      )
+      return
+    end
+  end
+
+  def update
+    temp_body = params[:body]
+    temp_title = params[:title]
+    temp_url = params[:url]
+    id = params[:id]
+
+    request_body = {}
+    request_body[:body] = temp_body if temp_body
+    request_body[:title] = temp_title if temp_title
+    request_body[:url] = temp_url if temp_url
+
+    if !id
+      render(
+        json: {
+          message: "Empty id or body"
+        },
+        status: :bad_request
+      )
+      return
+    end
+
+    updated_record = Content.find(id)
+    #.compact is used to skip nil values from hash
+    updated_record.update(request_body.compact)
+    if updated_record.persisted?
+      render(
+        json: {
+          message: "Data updated successfully",
+          data: updated_record
+        }
+      )
+      return
+    else
+      render(
+        json: {
+          message: "Data could not be updated"
+        },
+        status: :unprocessed_entity
+      )
+    end
+  end
+
+  def destroy
+    id = params[:id]
+    if !id
+      render(
+        json: {
+          message: "Id is empty"
+        },
+        status: :bad_request
+      )
+    end
+
+    record = Content.delete(id)
+    if record > 0
+      render(
+        json: {
+          message: "Record deleted successfully",
+          records: Content.all
+        }
+      )
       return
     end
   end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
   get "/api/contents", to: "contents#index"
   post "/api/contents", to: "contents#create"
+  put "/api/contents/:id", to: "contents#update"
+  delete "/api/contents/:id", to: "contents#destroy"
 end
